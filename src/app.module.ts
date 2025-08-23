ca95ffcd98323d0d2d46fa64ecab66e7a39f827a
Mark Enfermo <markanthony.enfermo@gmail.com>
2025-08-23 10:33:29 +0800

    refactor: rename project to 'server' and remove unused AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -8,7 +6,5 @@ import { StoreModule } from './store/store.module';
 
 @Module({
   imports: [AuthModule, UsersModule, PrismaModule, StoreModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
