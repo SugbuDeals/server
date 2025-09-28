c7ea1edd7ec2b9c6310d68a9f7e6e783c3ef5c81
Mark Enfermo <markanthony.enfermo@gmail.com>
2025-09-29 03:10:33 +0800

    feat: add Promotion module with controller, service, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,17 @@ import { PrismaModule } from './prisma/prisma.module';
 import { StoreModule } from './store/store.module';
 import { ProductModule } from './product/product.module';
 import { AiModule } from './ai/ai.module';
+import { PromotionModule } from './promotion/promotion.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule, StoreModule, ProductModule, AiModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    PrismaModule,
+    StoreModule,
+    ProductModule,
+    PromotionModule,
+    AiModule,
+  ],
 })
 export class AppModule {}
